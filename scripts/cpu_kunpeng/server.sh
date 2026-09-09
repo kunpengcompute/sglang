72acf37d79978a53f8bfce7a02bb15b730510a17
Li.Xing <lix930701@gmail.com>
2026-09-09 09:59:18 +0800

    feat(cpu_kunpeng): Add option to disable radix cache

--- scripts/cpu_kunpeng/server.sh
+++ scripts/cpu_kunpeng/server.sh
@@ -86,6 +86,11 @@ if [[ -n "${INIT_EXPERT_LOCATION:-}" ]]; then
     BASE_ARGS+=(--init-expert-location "$INIT_EXPERT_LOCATION")
 fi
 
+# Disable radix cache
+if [[ "$SGLANG_DISABLE_RADIX_CACHE" == "1" ]]; then
+    BASE_ARGS+=(--disable-radix-cache)
+fi
+
 # Disable overlap schedule
 if [[ "$SGLANG_ENABLE_OVERLAP" == "0" ]]; then
     BASE_ARGS+=(
